d049007d3ac99720ebec7d2a3f21acdc8df42c53
Carter B. Smith <>
2014-07-12 15:51:27 -0500

    menu overlay

---
+++ public/assets/scripts/application.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+    $('#menuOverlay').toggle();
+
+    $('#menu-wrap').on('click', function(){
+    	//alert('asdf');
+    	$('#menuOverlay').toggle();
+      	//$('#menuOverlay').css('display', 'block');
+
+    });
+
+    $('#menuOverlay').on('click', function(){
+    	$('#menuOverlay').toggle();
+    })
+
+});
